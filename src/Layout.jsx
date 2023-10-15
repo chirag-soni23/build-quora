417174519551eeb1eb92f9756574e173c5f9c2bd
Chirag <csoni0693@gmail.com>
2023-10-15 09:28:06 +0530

    Added javascript file

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Quoraheader from './Components/Header/Quoraheader'
-import Sidebar from './Components/Contents/Sidebar'
 import "./App.css"
 function Layout() {
   return (
@@ -11,7 +10,7 @@ function Layout() {
       <div className="quora_contents">
         <div className='quora_content'>
         <Outlet/>
-        {/* <Sidebar/> */}
+     
         </div>
       </div>
     </div>
