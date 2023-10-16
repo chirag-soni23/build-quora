1ebfef9e49b01495df41f1f230d2d40ba55aeffd
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-10-16 12:34:04 +0530

    Added react file

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Feed from '../Contents/Feed'
-import Widget from '../Contents/Widget'
-import Sidebar from '../Contents/Sidebar'
+import React from 'react';
+import Feed from '../Contents/Feed'; // Import the Feed component
+import Widget from '../Contents/Widget'; // Import the Widget component
+import Sidebar from '../Contents/Sidebar'; // Import the Sidebar component
+
 function Home() {
   return (
-    <div className = 'quora'>
+    <div className='quora'>
       <div className='quora_content'>
-        <Sidebar/>
-   <Feed/>
-   <Widget/>
-
+        <Sidebar /> {/* Render the Sidebar component for navigation and user information */}
+        <Feed /> {/* Render the Feed component for displaying posts and content */}
+        <Widget /> {/* Render the Widget component for additional content or widgets */}
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home; // Export the Home component for use as the main entry point of your application
--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -1,84 +1,76 @@
-import React from 'react'
-import "../css/Notifications.css"
-import { notifications } from '../Constant/Constant'
-
-
+import React from 'react';
+import "../css/Notifications.css";
+import { notifications } from '../Constant/Constant'; // Import notification data from a Constant file
 
 function Notification() {
   return (
     <div className="notification-wrapper">
-    <div className="notify-wrap">
-      <div className="notify-wrap1">
+      <div className="notify-wrap">
+        <div className="notify-wrap1">
+          {/* Left section for filtering notifications */}
           <div className="notifying1"></div>
           <div className="notifying2">
-              <div className="filter">Filters</div>
-           
-              <div className="notify">All Notifications</div>
-              <div className="notify1">
-                
-                      <div>Stories</div>
-                      <div>Questions</div>
-                      <div>Spaces</div>
-                      <div>People updates</div>
-                      <div>Comments and mentions</div>
-                      <div>Upvotes</div>
-                      <div>Your content</div>
-                      <div>Your Profile</div>
-                      <div>Announcements </div>
-                      <div>Earnings</div>
-                      <div>Subscription</div>
-                
+            <div className="filter">Filters</div>
+            <div className="notify">All Notifications</div>
+            <div className="notify1">
+              {/* List of notification filters */}
+              <div>Stories</div>
+              <div>Questions</div>
+              <div>Spaces</div>
+              <div>People updates</div>
+              <div>Comments and mentions</div>
+              <div>Upvotes</div>
+              <div>Your content</div>
+              <div>Your Profile</div>
+              <div>Announcements</div>
+              <div>Earnings</div>
+              <div>Subscription</div>
+            </div>
+          </div>
+        </div>
+        <div className="notify-wrap2">
+          <div className="notifywrap-main">
+            <div className="notifywrap-main1">
+              Notifications
+            </div>
+            <div className="notifywrap-main2">Settings</div>
+          </div>
+          {/* Render notifications based on the 'notifications' data array */}
+          {notifications.map((data) => {
+            return (
+              <div className="main" key={data.id}>
+                <div className='side'>
+                  <img src={data.logoImage} alt="" />
+                </div>
+                <div style={{ display: 'flex', flexDirection: "column", gap: "10px" }}>
+                  <div className='side2'>
+                    {/* Display notification details */}
+                    Highlighted in <span>{data.title}</span>. Posted by <span>{data.name}</span> <span>{data.timestamp}</span>
+                  </div>
+                  {data.message}
+                  <div className='side22'>
+                    {/* Additional elements related to notifications */}
+                  </div>
+                </div>
+                <div className='side3'>
+                  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Z" class="icon_svg-stroke" stroke-width="1.5" stroke="#666" fill="none"></path>
+                  </svg>
+                </div>
               </div>
-          </div>
-      </div>
-      <div className="notify-wrap2">
-           <div className="notifywrap-main">
-          <div className="notifywrap-main1">
-            Notifications
-          </div>
-          <div className="notifywrap-main2">Settings</div>
+            );
+          })}
         </div>
-        {/* <div className="notify-img">
-            <img src="https://qsf.fs.quoracdn.net/-4-ans_frontend_assets.images.empty_states.no_notification_lightmode.png-26-9e0ef76620dd73d3.png" alt="" />
-          </div> */}
-          {/* <div className="notify-text">No New Notifications</div>
-          <div className="notify-text1">Notifications you received in the last 30 days will show up here.</div> */}
-         {notifications.map((data)=>{     
-          return  <div className="main">
-            <div className='side' key={data.id}>
-              <img src={data.logoImage}alt="" />
-            </div>
-            <div style={{display:'flex', flexDirection:"column",gap:"10px"}}>
-            <div className='side2'>
-   Highlighted in <span>{data.title}</span>.Posted by  <span>{data.name}</span> <span>{data.timestamp}</span>
-</div>
-{data.message}
-
-     <div className='side22'>
-
-
-            </div>
-            </div>
-            <div className='side3' >
-            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Z" class="icon_svg-stroke" stroke-width="1.5" stroke="#666" fill="none"></path></svg>
-
-            </div>
-          </div>
-})}
-
+        <div className="notify-wrap3">
+          {/* Additional section, if any */}
         </div>
-      <div className="notify-wrap3">
-        
       </div>
-   
-  
-   </div>
-   </div>
-
-    )
+    </div>
+  );
 }
 
-export default Notification
+export default Notification; // Export the Notification component for use in your application
+
 
 
 
